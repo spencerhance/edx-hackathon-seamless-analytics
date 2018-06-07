93d80376d8699ed676eeb8d94a477558709421f0
Isabel <isabelgk@users.noreply.github.com>
2018-06-07 10:35:12 -0400

    Add logo to header

--- seamless-analytics-frontend/src/components/Header/index.jsx
+++ seamless-analytics-frontend/src/components/Header/index.jsx
@@ -1,19 +1,50 @@
-import React from 'react'
-import './Header.scss';
 
-class Header extends React.Component {
-    
-    render(){
-        return (
-            <header>
-                <className name="title">
-                <h1 align= "center">Seamless Analytics</h1>
-                <hr width="100%" color="black"/>
-                </className>
-            </header>
-        );
 
-    }
+import React from "react";
+import logo from '../../seamless-analytics-logo.svg';
+import { Menu, Header } from "semantic-ui-react";
+// import "./Header.scss";
+
+class HeaderComponent extends React.Component {
+  state = {};
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name });
+
+  render() {
+    const { activeItem } = this.state;
+
+    return (
+      <Menu stackable>
+        <Menu.Item>
+          <img height="32px" src={logo} alt="Logo" />
+        </Menu.Item>
+
+        <Menu.Item
+          name="features"
+          active={activeItem === "features"}
+          onClick={this.handleItemClick}
+        >
+          @ edX
+        </Menu.Item>
+
+        {/* <Menu.Item
+          name="testimonials"
+          active={activeItem === "testimonials"}
+          onClick={this.handleItemClick}
+        >
+          Testimonials
+        </Menu.Item>
+
+        <Menu.Item
+          name="sign-in"
+          active={activeItem === "sign-in"}
+          onClick={this.handleItemClick}
+        >
+          Sign-in
+        </Menu.Item> */}
+      </Menu>
+    );
+  }
 }
 
-export default Header;
+export default HeaderComponent;
